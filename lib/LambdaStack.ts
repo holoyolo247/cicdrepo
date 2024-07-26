bf0e0789317fb4ea4c22659f310d7c2a03f3cf85
Kosa Ko <kosa.wl.ko@pccw.com>
2024-07-26 11:59:22 +0800

    Add test stage and lambda stack

---
+++ lib/LambdaStack.ts
@@ -0,0 +1,18 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+
+interface LambdaStackProps extends StackProps{
+    stageName?: string
+}
+
+
+
+export class LambdaStack extends Stack{ // will recevie a stage name, so that our lambda knows which stage
+    constructor(scope:Construct, id:string, props:LambdaStackProps){
+        super(scope, id, props)
+
+
+
+    }
+}
--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this,'DemoPipeline',{
+    const pipeline = new CodePipeline(this,'DemoPipeline',{
       pipelineName: 'DemoPipeline',
       synth: new ShellStep('Synth',{
         input: CodePipelineSource.gitHub('holoyolo247/cicdrepo', 'main'),
@@ -17,5 +18,10 @@ export class TestStack extends cdk.Stack {
         ]
       })
     })
+
+
+    const testStage = pipeline.addStage(new PipelineStage(this,'PipelineTestStage',{
+      stageName:'test'
+    }))
   }
 }
