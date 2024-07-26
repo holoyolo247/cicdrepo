0f70e5a73dc6218a0e6dcf3b42aab7048b8259a8
Kosa Ko <kosa.wl.ko@pccw.com>
2024-07-26 11:16:11 +0800

    pipeline init

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,15 @@ export class TestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this,'DemoPipeline',{
+      pipelineName: 'DemoPipeline',
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub('holoyolo247/cicdrepo', 'main'),
+        commands:[
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
