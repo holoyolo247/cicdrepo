a722e6d5a69bbebd4658665fbaa1fabdaa790448
Kosa Ko <kosa.wl.ko@pccw.com>
2024-07-29 11:03:15 +0800

    break body change

--- test/test.test.ts
+++ test/test.test.ts
@@ -4,6 +4,13 @@ describe('Hello describe test suite',()=>{
     test('handler should return 200',async()=>{
         const result = await handler({},{})
         expect(result.statusCode).toBe(200)
+       
+
+    })
+    test('handler should return body = Hello',async()=>{
+        const result = await handler({},{})
+        expect(result.body).toBe('Hello')
+       
 
     })
 })
